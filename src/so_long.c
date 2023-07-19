a57e5a39d9342150633e4471144efb4b5a16e913
Petru Cazac <pcazac@pop-os.localdomain>
2023-07-19 07:43:48 +0200

    Starting to put images

--- src/so_long.c
+++ src/so_long.c
@@ -33,20 +33,27 @@ int	main(int argc, char *argv[])
 {
 	char 	**map;
 	mlx_t	*mlx;
-	mlx_image_t		*image;
-	mlx_texture_t	*texture;
+	image_p		*image;
+	texture_p	*texture;
 	
 	check_all(argc);
 	map = parser(argv[1]);
 	find_path(map);
 	mlx = mlx_init(WIDTH, HEIGHT, "GAME", 1);
-	texture = mlx_load_png("Textures/Tile_Textures/Tile/Walls.png");
-	image = mlx_texture_to_image(mlx, texture);
+	texture = malloc(1*sizeof(texture_p));
+	image = malloc(1*sizeof(image_p));
+	texture->t_walls = mlx_load_png("Textures/Tile_Textures/Tile/Walls.png");
+	texture->t_ground = mlx_load_png("Textures/Tile_Textures/Wood/Wood.png");
+	image->i_walls = mlx_texture_to_image(mlx, texture->t_walls);
+	image->i_ground = mlx_texture_to_image(mlx, texture->t_ground);
+	mlx_resize_image(image->i_walls, 50, 50);
+	mlx_resize_image(image->i_ground, 50, 50);
 	if (mlx == NULL)
 		exit(EXIT_FAILURE);
 	else
 	{
-		mlx_image_to_window(mlx, image, 0, 0);
+		mlx_image_to_window(mlx, image->i_walls, 0, 0);
+		mlx_image_to_window(mlx, image->i_ground, 50, 0);
 		// mlx_image_to_window(mlx, image->i_ground, 0, 0);
 		mlx_loop(mlx);
 		mlx_close_window(mlx);
